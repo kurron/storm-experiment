b0e81de68f2dae5e982c67ce64577256e5b6d23d
Ron Kurr <rkurr@transparent.com>
2015-02-20 20:53:23 -0500

    Initial check in

---
+++ src/main/groovy/storm/StormApplication.groovy
@@ -0,0 +1,12 @@
+package storm
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class StormApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run StormApplication, args
+    }
+}
---
+++ src/test/groovy/storm/StormApplicationTests.groovy
@@ -0,0 +1,16 @@
+package storm
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.SpringApplicationConfiguration
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+
+@RunWith(SpringJUnit4ClassRunner)
+@SpringApplicationConfiguration(classes = StormApplication)
+class StormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
